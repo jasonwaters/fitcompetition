8b8021d2561c8d096bec90f553b4b6b51bc0b70b
Jason Waters <jason@myheck.net>
2014-02-02 11:31:25 -0700

    travis ci integration

---
+++ local_settings_travis.py
@@ -0,0 +1,46 @@
+DEBUG = False
+SSL_ENABLED = True
+
+SECRET_KEY = 'travis-ci'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fc_test_db',
+        'USER': 'travis',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'STORAGE_ENGINE': 'MyISAM',
+        }
+}
+
+PAYPAL_ACCOUNT_EMAIL = 'not@valid.net'
+SERVER_EMAIL = "not@valid.net"
+
+AWS_ACCESS_KEY_ID = ''
+AWS_SECRET_ACCESS_KEY = ''
+AWS_STORAGE_BUCKET_NAME = ''
+
+MAILCHIMP_API_KEY = ''
+MAILCHIMP_LIST_ID = ''
+
+SOCIAL_AUTH_RUNKEEPER_KEY = ''
+SOCIAL_AUTH_RUNKEEPER_SECRET = ''
+
+SOCIAL_AUTH_MAPMYFITNESS_KEY = ''
+SOCIAL_AUTH_MAPMYFITNESS_SECRET = ''
+
+DISQUS_SHORTNAME = "invalid"
+
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = ['localhost']
+BASE_URL = 'http://localhost'
+
+#celery broker (RabbitMQ)
+BROKER_URL = "amqp://guest:guest@localhost//"
+
+GOOGLE_ANALYTICS_TRACKING_ID = ''
+
+LOCALE = 'en_US.utf8'
