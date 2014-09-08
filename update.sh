38bf94e27fa0c2e1ad9430d4ae8b326da518d03d
Jason Waters <jason@myheck.net>
2014-09-07 20:51:27 -0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,5 @@
 source env/bin/activate
 pip install -r requirements.pip
-rm -rf static/*
 python manage.py collectstatic --noinput
 python manage.py syncdb --noinput
 python manage.py migrate --noinput
