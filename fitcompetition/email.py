05462f50517cb03a9886022d63c451ad18995ab3
Jason Waters <jason@myheck.net>
2015-05-15 19:51:47 -0600

    wrapped email Context and now logging errors.

--- fitcompetition/email.py
+++ fitcompetition/email.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import logging
 from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
+from django.template import Context
 from django.template.loader import render_to_string
 from fitcompetition.util.ListUtil import createListFromProperty
 import pytz
@@ -18,11 +20,11 @@ class EmailFactory(object):
             playerEmail = Email(to=user.email, subject="Your cash is on it's way!")
             systemEmail = Email(to=getattr(settings, "PAYPAL_ACCOUNT_EMAIL"), subject="%s is cashing out" % user.first_name)
 
-            context = {
+            context = Context({
                 'user': user,
                 'email': paypalEmailAddress,
                 'value': value
-            }
+            })
 
             playerEmail.html('email/cashWithdrawal_player.html', context)
             systemEmail.html('email/cashWithdrawal_system.html', context)
@@ -36,11 +38,11 @@ class EmailFactory(object):
 
             approvedTypes = challenge.approvedActivities.all()
 
-            context = {
+            context = Context({
                 'challenge': challenge,
                 'user': user,
                 'approvedActivities': createListFromProperty(approvedTypes, 'name'),
-            }
+            })
 
             email.html("email/challenge_joined.html", context)
             email.send(self.FROM)
@@ -50,11 +52,11 @@ class EmailFactory(object):
 
         approvedTypes = challenge.approvedActivities.all()
 
-        context = {
+        context = Context({
             'challenge': challenge,
             'user': user,
             'approvedActivities': createListFromProperty(approvedTypes, 'name'),
-        }
+        })
 
         email.html("email/challenge_start.html", context)
         email.send(self.FROM)
@@ -62,11 +64,11 @@ class EmailFactory(object):
     def challengeHalf(self, user, challenge):
         email = Email(to=user.email, subject="The challenge is half over!")
 
-        context = {
+        context = Context({
             'challenge': challenge,
             'user': user,
             'achievedGoal': challenge.getAchievedGoal(user)
-        }
+        })
 
         email.html("email/challenge_half.html", context)
         email.send(self.FROM)
@@ -74,13 +76,13 @@ class EmailFactory(object):
     def challengeEnd(self, user, challenge, upcomingChallenges, isWinner):
         email = Email(to=user.email, subject="The challenge is over!")
 
-        context = {
+        context = Context({
             'challenge': challenge,
             'user': user,
             'achievedGoal': challenge.getAchievedGoal(user),
             'isWinner': isWinner,
             'upcomingChallenges': upcomingChallenges
-        }
+        })
 
         email.html("email/challenge_end.html", context)
         email.send(self.FROM)
@@ -88,11 +90,11 @@ class EmailFactory(object):
     def cashDeposit(self, transaction, account, user):
         email = Email(to=user.email, subject="Your payment was received")
 
-        context = {
+        context = Context({
             'transaction': transaction,
             'user': user,
             'account': account
-        }
+        })
 
         email.html("email/cashDeposit.html", context)
         email.send(self.FROM)
@@ -100,6 +102,7 @@ class EmailFactory(object):
 
 class Email(object):
     def __init__(self, to, subject):
+        self.logger = logging.getLogger(__name__)
         self.to = to
         self.subject = subject
         self._html = None
@@ -112,6 +115,7 @@ class Email(object):
         try:
             output = render_to_string(template, context)
         except Exception, e:
+            self.logger.error(e)
             output = ""
 
         return output
