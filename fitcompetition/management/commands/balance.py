4f3a4e3641ec1db3f2ed269cf81776faa81b9b31
Jason Waters <jason@myheck.net>
2015-08-29 10:02:42 -0600

    balance command

---
+++ fitcompetition/management/commands/balance.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from fitcompetition.models import Transaction
+from django.db.models import Sum
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+
+        result = Transaction.objects.all().aggregate(balance=Sum('amount'))
+
+        return getAttr(result, 'balance')
